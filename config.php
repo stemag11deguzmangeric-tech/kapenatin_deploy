2a6add009a14fa724d8ac81f505d0217212e27f7
herardoww <stemag11.deguzmangeric@gmail.com>
2026-05-03 18:54:15 +0800

    Fix spacing in database name assignment

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
 $host     = "sql107.infinityfree.com"; // replace with your DB host
 $user     = "if0_41817308";     // replace with your DB username
 $password = "vIiibK8o91uIEl";        // replace with your DB password
-$db_name  = "if0_41817308_kapenatin ";     // replace with your DB name
+$db_name  = "if0_41817308_kapenatin";     // replace with your DB name
 
 $mysqli = new mysqli($host, $user, $password, $db_name);
 
